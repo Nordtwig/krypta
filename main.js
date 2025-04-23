777be86b07493c979d4507178d8bb2bcd28c03a2
Noah <nordqvist.gaming@gmail.com>
2025-04-23 13:56:47 +0200

    add basic path indicator

--- main.js
+++ main.js
@@ -5,21 +5,23 @@ fileName = document.getElementById("fileName")
 fileContents = document.getElementById("fileContents")
 
 let pathName = path.join(__dirname, "/")
-// let directoryPath = path.join("/development/bohus")
 
 explorer = document.getElementById("explorer")
 
-fs.readdir("/home/noah/development/krypta/", { withFileTypes: true }, function(error, files) {
+const pathElement = document.getElementById("path")
+pathElement.textContent = pathName
+
+fs.readdir(pathName, { withFileTypes: true }, function(error, files) {
     if (error)
         return console.log("Unable to scan directory: " + error);
 
     files.forEach(function(file) {
         const newItem = document.createElement("li")
         const newAnchor = document.createElement("a")
-        const newIcon = document.createElement("i")
+        const newIcon = document.createElement("img")
 
-        if (fs.statSync("/home/noah/development/krypta/" + file.name).isFile()) {
-            fs.readFile("/home/noah/development/krypta/" + file.name, function(error, data) {
+        if (fs.statSync(pathName + file.name).isFile()) {
+            fs.readFile(pathName + file.name, function(error, data) {
                 newIcon.className = "file-icon"
             })
         } else {
